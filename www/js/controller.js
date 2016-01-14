2e40da4393f5295a6712de0a6f10f4aee3a29005
647 <Liu@airmatoAir.gateway>
2016-01-14 12:02:32 +1100

    basic calculator

---
+++ www/js/controller.js
@@ -0,0 +1,208 @@
+angular.module('starter').controller('CalCtrl', ['$scope', function ($scope) {
+    $scope.pendingValue = null;
+    $scope.newNumber = true;
+    $scope.runningTotal = null;
+    $scope.operationToken = "";
+
+    $scope.display = 0;
+
+    var ADD = "adding";
+    var SUBTRACT = "subtracting";
+    var ADD_TOKEN = "+";
+    var SUBTRACT_TOKEN = "-";
+    var DIVIDE = "dividing"
+    var DIVIDE_TOKEN = "/";
+    var MULTIPLY = "multiplying"
+    var MULTIPLY_TOKEN = "*";
+
+    $scope.enterNumber = function (number) {
+      if ($scope.display == "0" || $scope.newNumber) {
+          $scope.display = number;
+          $scope.newNumber = false;
+      } else {
+          $scope.display += String(number);
+      }
+      $scope.pendingValue = parseFloat($scope.display);
+    };
+
+    $scope.add = function () {
+        if ($scope.pendingValue) {
+            if ($scope.runningTotal && $scope.pendingOperation == ADD) {
+                $scope.runningTotal += $scope.pendingValue;
+            } else {
+                $scope.runningTotal = $scope.pendingValue;
+            }
+        }
+        setOperationToken(ADD);
+        setDisplay(String($scope.runningTotal));
+        $scope.pendingOperation = ADD;
+        $scope.newNumber = true;
+        $scope.pendingValue = null;
+    };
+
+    $scope.subtract = function () {
+        if ($scope.pendingValue) {
+            if ($scope.runningTotal && ($scope.pendingOperation == SUBTRACT)) {
+                $scope.runningTotal -= $scope.pendingValue;
+            } else {
+                $scope.runningTotal = $scope.pendingValue;
+            }
+        }
+        setOperationToken(SUBTRACT);
+        setDisplay(String($scope.runningTotal));
+        $scope.pendingOperation = SUBTRACT;
+        $scope.newNumber = true;
+        $scope.pendingValue = null;
+    };
+
+    $scope.divide = function () {
+        if ($scope.pendingValue) {
+            if ($scope.runningTotal && $scope.pendingOperation == DIVIDE) {
+                $scope.runningTotal /= $scope.pendingValue;
+            } else {
+                $scope.runningTotal = $scope.pendingValue;
+            }
+        }
+        setOperationToken(DIVIDE);
+        setDisplay(String($scope.runningTotal));
+        $scope.pendingOperation = DIVIDE;
+        $scope.newNumber = true;
+        $scope.pendingValue = null;
+    };
+
+    $scope.multiply = function () {
+        if ($scope.pendingValue) {
+            if ($scope.runningTotal && $scope.pendingOperation == MULTIPLY) {
+                $scope.runningTotal *= $scope.pendingValue;
+            } else {
+                $scope.runningTotal = $scope.pendingValue;
+            }
+        }
+        setOperationToken(MULTIPLY);
+        setDisplay(String($scope.runningTotal));
+        $scope.pendingOperation = MULTIPLY;
+        $scope.newNumber = true;
+        $scope.pendingValue = null;
+    };
+
+    $scope.calculate = function () {
+        if (!$scope.newNumber) {
+            $scope.pendingValue = parseFloat($scope.display);
+            $scope.lastValue = $scope.pendingValue;
+        }
+        if ($scope.pendingOperation == ADD) {
+            $scope.runningTotal += $scope.pendingValue;
+            $scope.lastOperation = ADD;
+        } else if ($scope.pendingOperation == SUBTRACT) {
+            $scope.runningTotal -= $scope.pendingValue;
+            $scope.lastOperation = SUBTRACT;
+        } else if ($scope.pendingOperation == DIVIDE) {
+            $scope.runningTotal /= $scope.pendingValue;
+            $scope.lastOperation = DIVIDE;
+        } else if ($scope.pendingOperation == MULTIPLY) {
+            $scope.runningTotal *= $scope.pendingValue;
+            $scope.lastOperation = MULTIPLY;
+        } else {
+            if ($scope.lastOperation) {
+                if ($scope.lastOperation == ADD) {
+                    if ($scope.runningTotal) {
+                        $scope.runningTotal += $scope.lastValue;
+                    } else {
+                        $scope.runningTotal = 0;
+                    }
+                } else if ($scope.lastOperation == SUBTRACT) {
+                    if ($scope.runningTotal) {
+                        $scope.runningTotal -= $scope.lastValue;
+                    } else {
+                        $scope.runningTotal = 0;
+                    }
+                }
+                } else if ($scope.lastOperation == DIVIDE) {
+                    if ($scope.runningTotal) {
+                        $scope.runningTotal /= $scope.lastValue;
+                    } else {
+                        $scope.runningTotal = 0;
+                    }
+                } else if ($scope.lastOperation == MULTIPLY) {
+                    if ($scope.runningTotal) {
+                        $scope.runningTotal *= $scope.lastValue;
+                    } else {
+                        $scope.runningTotal = 0;
+                    }
+            } else {
+                $scope.runningTotal = 0;
+            }
+        }
+        setDisplay($scope.runningTotal);
+        setOperationToken();
+        $scope.pendingOperation = null;
+        $scope.pendingValue = null;
+        console.log($scope.runningTotal);
+    };
+
+    setDisplay = function (outputString) {
+      $scope.display = outputString;
+      $scope.newNumber = true;
+      console.log($scope.display)
+    };
+
+    setOperationToken = function (operation) {
+        if (operation == ADD) {
+            $scope.operationToken = ADD_TOKEN;
+        } else if (operation == SUBTRACT) {
+            $scope.operationToken = SUBTRACT_TOKEN;
+        } else {
+            $scope.operationToken = "";
+        }
+    };
+
+    $scope.operationButtonClick = function (operation) {
+      $scope.display = operation;
+    }
+
+    /** Clears the display box **/
+    $scope.clear = function (clear) {
+      $scope.display = 0;
+    }
+
+    $scope.setDisplay = function (outputString) {
+      $scope.display = outputString;
+      $scope.newNumber = true;
+    };
+
+    // Functions for the top two rows
+    $scope.pow = function() {
+      $scope.display = ($scope.display)*($scope.display);
+    }
+
+    $scope.sin = function() {
+      $scope.display = Math.sin($scope.display*(Math.PI / 180));
+    }
+
+    $scope.cos = function() {
+      $scope.display = Math.cos($scope.display*(Math.PI / 180));
+    }
+
+    $scope.tan = function() {
+      $scope.display = Math.tan($scope.display*(Math.PI / 180));
+    }
+
+    $scope.sqrt = function() {
+      $scope.display = Math.sqrt($scope.display);
+    }
+
+    $scope.asin = function() {
+      $scope.display = Math.asin($scope.display*(Math.PI / 180));
+    }
+
+    $scope.acos = function() {
+      $scope.display = Math.acos($scope.display*(Math.PI / 180));
+    }
+
+    $scope.atan = function() {
+      $scope.display = Math.atan($scope.display*(Math.PI / 180));
+    }
+
+  }]);
+
+
